e0c9a85c297fffa6062a3cd43c0bb8f13872d3ce
Filipecoelho999 <95039416+Filipecoelho999@users.noreply.github.com>
2021-12-04 23:22:50 +0000

    Add files via upload
    Conclusão das conversões em JavaScript

--- script.js
+++ script.js
@@ -30,4 +30,33 @@ function getopcao1() {
         var num2 = parseInt(hex,16);
         document.getElementById("resultado").innerHTML =(num2.toString(2));
     }
+    if(document.getElementById("opcao1").value=='1' && document.getElementById("opcao2").value=='16'){
+        var num2 = parseInt(num1,2);
+        document.getElementById("resultado").innerHTML =(num2.toString(16));
+    }     
+    if(document.getElementById("opcao1").value=='2' && document.getElementById("opcao2").value=='16'){
+        document.getElementById("resultado").innerHTML =(num1.toString(16));
+    }     
+    if(document.getElementById("opcao1").value=='3' && document.getElementById("opcao2").value=='16'){ 
+        var num2 = parseInt(num1,8);         
+        document.getElementById("resultado").innerHTML =(num2.toString(16));     
+    }     
+    if(document.getElementById("opcao1").value=='4' && document.getElementById("opcao2").value=='16'){         
+        document.getElementById("resultado").innerHTML = hex     
+    }
+    if(document.getElementById("opcao1").value=='1' && document.getElementById("opcao2").value=='8'){         
+        var num2 = parseInt(num1,2);         
+        document.getElementById("resultado").innerHTML =(num2.toString(8));     
+    }     
+    if(document.getElementById("opcao1").value=='2' && document.getElementById("opcao2").value=='8'){         
+        document.getElementById("resultado").innerHTML =(num1.toString(8));     
+    }     
+    if(document.getElementById("opcao1").value=='3' && document.getElementById("opcao2").value=='8'){         
+        document.getElementById("resultado").innerHTML = num1     
+    }     
+    if(document.getElementById("opcao1").value=='4' && document.getElementById("opcao2").value=='8'){         
+        var num2 = parseInt(num1,16);         
+        document.getElementById("resultado").innerHTML =(num2.toString(8));
+    }
+
 }
